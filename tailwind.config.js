2a33727bbcff195a6b88e79357a906721d6aed37
Abel D <adq21791@gmail.com>
2023-07-30 19:24:54 +0200

    Aina mod cover

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       colors: {
         'primary': '#F7F2EC',
         'secondary': '#617C71'
+      },
+      fontFamily: {
+        'abel': ['Abel', 'sans-serif']
       }
     },
   },
